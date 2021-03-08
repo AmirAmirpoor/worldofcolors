42b99608236970ccb67e40a4026621fdddf2cc4c
amiramirpoor <22155aamirpoor@gmail.com>
2021-03-08 12:02:08 -0800

    set up basic routing by react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Switch, Route, Link } from "react-router-dom";
+
 import "./styles/global.css";
 
 function App() {
   return (
     <div>
-      <h1>WORLD OF COLORS</h1>
+      {/* test */}
+      <nav>
+        <Link to="/">home</Link>
+        <Link to="/generate">generate palette</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home page</h1>} />
+        <Route
+          exact
+          path="/generate"
+          render={() => <h1>generate new palette page</h1>}
+        />
+      </Switch>
     </div>
   );
 }
