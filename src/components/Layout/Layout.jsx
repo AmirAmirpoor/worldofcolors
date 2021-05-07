fab989579d3c7b98b679d8542f33df7566b37851
amir amirpoor <22155aamirpoor@gmail.com>
2021-05-08 00:46:15 +0430

    display navbar content on mobile (mobile nav)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -43,8 +43,40 @@ const Layout = ({ children }) => {
     else document.body.style.overflow = "auto";
   }, [isNavbarVisible]);
 
+  const navbarContent = () => {
+    return (
+      <>
+        <Link to="/" className={classes.navbar__logo}>
+          <h1>COLORS</h1>
+        </Link>
+
+        <ul className={classes.navbar__items}>
+          {NAVBAR_ITEMS.map((item) => (
+            <li key={item.id} className={classes.navbar__item}>
+              <NavLink
+                to={item.to}
+                className={classes.navbar__link}
+                activeClassName={classes.navbar__active}
+                exact
+                onClick={() => setNavbarVisible(false)}
+              >
+                {item.icon}
+                <span>{item.title}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        <p className={classes.navbar__footer}>MADE BY AMIR</p>
+      </>
+    );
+  };
+
   return (
     <div className={classes.layout}>
+      {/* NAVBAR ON DESKTOP */}
+      <div className={classes.layout__navbar}>{navbarContent()}</div>
+
       {/* MOBILE HEADER and SIDEBAR */}
       <header className={classes.mobile__header}>
         <Link to="/" className={classes.navbar__logo}>
@@ -60,39 +92,20 @@ const Layout = ({ children }) => {
           onClick={handleNavbarClick}
         >
           <nav className={classes.mobile__nav}>
-            <RoundButton icon={<CloseIcon />} onClick={closeNavbar} />
-            LOGO
-            <br />
-            LINKS
-            <br />
-            FOOTER
+            <div className={classes.mobile__nav__header}>
+              <Link
+                to="/"
+                className={classes.mobile__nav__logo}
+                onClick={() => setNavbarVisible(false)}
+              >
+                <h1>COLORS</h1>
+              </Link>
+              <RoundButton icon={<CloseIcon />} onClick={closeNavbar} />
+            </div>
+            {navbarContent()}
           </nav>
         </div>
       </header>
-
-      <div className={classes.layout__navbar}>
-        <Link to="/" className={classes.navbar__logo}>
-          <h1>COLORS</h1>
-        </Link>
-
-        <ul className={classes.navbar__items}>
-          {NAVBAR_ITEMS.map((item) => (
-            <li key={item.id} className={classes.navbar__item}>
-              <NavLink
-                to={item.to}
-                className={classes.navbar__link}
-                activeClassName={classes.navbar__active}
-                exact
-              >
-                {item.icon}
-                <span>{item.title}</span>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-
-        <p className={classes.navbar__footer}>MADE BY AMIR</p>
-      </div>
 
       <div className={classes.layout__content}>{children}</div>
     </div>
