fbc101c95d97208a4b48a894ac80b7e49417599a
amir amirpoor <22155aamirpoor@gmail.com>
2021-05-15 22:59:41 +0430

    produce random colors based on locked or unlocked colors

--- src/pages/Generate/Generate.jsx
+++ src/pages/Generate/Generate.jsx
@@ -1,16 +1,39 @@
+// redux stuff
+import { useSelector, useDispatch } from "react-redux";
+import { set_colors } from "../../store/actions/newPaletteActions";
+
 // components
 import Colors from "./Colors/Colors.jsx";
+
+// chroma
+import chroma from "chroma-js";
 
 // styles
 import classes from "./Generate.module.css";
 
 const Generate = () => {
+  const { colors } = useSelector((state) => state.newPalette);
+  const dispatch = useDispatch();
+
+  const genRandomColors = () => {
+    const randomColors = colors.map((color) =>
+      color.isLocked ? color : { ...color, value: chroma.random().hex() }
+    );
+
+    dispatch(set_colors(randomColors));
+  };
+
   return (
     <div className="container">
       <div className="main">
         <Colors />
       </div>
-      <div className="aside">ASIDE</div>
+      <div className="aside">
+        {/* FOR TEST RANDOM COLORS FUNCTION */}
+        <button className="btn" onClick={genRandomColors}>
+          random
+        </button>
+      </div>
     </div>
   );
 };
